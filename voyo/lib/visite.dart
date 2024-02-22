f0b5da0344be20c3baf2447275dca5a75add487b
gardannl <lucas.gardanne@etu-univ-grenoble-alpes.fr>
2024-02-22 15:53:04 +0100

    WIP viste demande page

--- voyo/lib/visite.dart
+++ voyo/lib/visite.dart
@@ -13,46 +13,172 @@ class VisitePage extends StatefulWidget {
 }
 
 class _VisitePageState extends State<VisitePage> {
+  String? _selectedOption = "";
+
+  var items = ['T1', "T2", "Villa"];
+
   @override
   Widget build(BuildContext context) {
     return AppGlobal.Menu(
-        Column(
-          children: [
-            Visitor("thomas", "thomas", "lyon", "13", "26"),
-            Row(children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8, right: 8),
-                  child: FloatingActionButton(
-                      backgroundColor: AppGlobal.secondaryColor,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      onPressed: null,
-                      child: const Expanded(
-                          child: Text(
-                        "Voir profil",
-                        style: TextStyle(color: Colors.black),
-                      ))),
-                ),
-              ),
-            ]),
-            Row(children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 8,right: 8),
-                    child:FloatingActionButton(
-                        backgroundColor: AppGlobal.secondaryColor,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+        SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+              autovalidateMode: AutovalidateMode.always,
+              onChanged: () {
+                Form.of(primaryFocus!.context!).save();
+              },
+                child: Wrap(
+                  children: [
+                    Padding(padding: const EdgeInsets.all(8.0),
+                      child:DropdownButtonFormField(
+                        decoration: InputDecoration(
+                          hintText: 'Type de logement',
+                          filled: true,
+                          fillColor: AppGlobal.buttonback,
                         ),
-                        onPressed: null,
-                        child: const Expanded( child : Text("Valider et payer",style: TextStyle(color: Colors.black),))
+                        dropdownColor: Colors.amber,
+                        items: items.map((items){
+                          return DropdownMenuItem(value : items, child: Text(items));
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _selectedOption = newValue;
+                          });
+                        }
+                      ),
                     ),
-                  ),
-                ),
-                ]),
-          ],
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0), // ajustez le padding selon vos besoins
+                            child: TextFormField(
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                hintText: "Ville",
+                                filled: true,
+                                fillColor: AppGlobal.buttonback,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0), // ajustez le padding selon vos besoins
+                            child: TextFormField(
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                hintText: "Rue",
+                                filled: true,
+                                fillColor: AppGlobal.buttonback,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0), // ajustez le padding selon vos besoins
+                            child: TextFormField(
+                              textAlign: TextAlign.left,
+                              decoration: InputDecoration(
+                                hintText: "code postale",
+                                filled: true,
+                                fillColor: AppGlobal.buttonback,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Visitor("thomas", "thomas", "lyon", "13", "26"),
+                    Row(children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8, right: 8),
+                          child: FloatingActionButton(
+                              backgroundColor: AppGlobal.secondaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              onPressed: null,
+                              child: const Expanded(
+                                  child: Text(
+                                    "Voir profil",
+                                    style: TextStyle(color: Colors.black),
+                                  ))),
+                        ),
+                      ),
+                    ]),
+
+                    const Padding(padding: EdgeInsets.all(10.0),
+                      child: Text(
+                        'Point à vérifier:',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          textAlign: TextAlign.left,
+                          decoration: InputDecoration(
+                            hintText: "ex: WC séparé",
+                            filled: true,
+                            fillColor: AppGlobal.buttonback,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Center(
+                      child:
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8,bottom: 8),
+                          child:FloatingActionButton(
+                            backgroundColor: AppGlobal.primaryColor,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            onPressed: null,
+                            child:
+                            const Expanded(
+                              child : Text(
+                                '+',
+                                style: TextStyle(
+                                  fontSize: 25.0,
+                                  color: Colors.black
+                                ),
+                              )
+                            )
+                          ),
+                        ),
+                      ),
+
+                    Row(children: [
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 8,right: 8),
+                          child:FloatingActionButton(
+                              backgroundColor: AppGlobal.secondaryColor,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                              onPressed: null,
+                              child: const Expanded( child : Text("Valider et payer",style: TextStyle(color: Colors.black),))
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ]),
+              ),
+            ],
+          ),
         ),
         widget,
     context);
