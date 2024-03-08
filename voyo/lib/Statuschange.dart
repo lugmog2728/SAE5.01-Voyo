efdbbbdbfe3d418fa3db5652fbfcb1ff29b7cc7f
bensalay <yanis.ben-salah@etu.univ-grenoble-alpes.fr>
2024-03-08 09:20:19 +0100

    Version un peu plus propre de la emande de changement de statut
    WIP -> visuel à changer mais fonctionnel

--- voyo/lib/Statuschange.dart
+++ voyo/lib/Statuschange.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Ajout de cette ligne pour importer FilteringTextInputFormatter
+import 'globals.dart' as AppGlobal;
 
 class ChangementStatutPage extends StatefulWidget {
   const ChangementStatutPage({Key? key, required this.title}) : super(key: key);
@@ -41,140 +43,135 @@ class _ChangementStatutPageState extends State<ChangementStatutPage> {
     // Action de confirmation à implémenter ici
   }
 
+  TextFormField buildHourInput(TextEditingController controller) {
+    return TextFormField(
+      controller: controller,
+      inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'[0-9:]'))],
+      decoration: InputDecoration(
+        labelText: "Horaire",
+        suffixText: 'heure',
+      ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Veuillez saisir une heure valide';
+        }
+        // Vérifier le format de l'heure ici
+        final validFormat = RegExp(r'^[0-2]?[0-9]:[0-5][0-9]$').hasMatch(value);
+        if (!validFormat) {
+          return 'Format d\'heure invalide. Utilisez HH:MM';
+        }
+        return null;
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: SingleChildScrollView(
+    return AppGlobal.Menu(
+      SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  side: BorderSide(color: Colors.yellow, width: 2),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Informations',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24.0),
+                  textAlign: TextAlign.center,
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'Informations',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-                        textAlign: TextAlign.center,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    TextFormField(
+                      controller: telephoneController,
+                      decoration: InputDecoration(
+                        labelText: "Numéro de téléphone",
                       ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        controller: telephoneController,
-                        decoration: InputDecoration(
-                          labelText: "Numéro de téléphone",
-                          filled: true,
-                          fillColor: Colors.blue[50], // Couleur de fond du champ de texte
-                        ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      controller: ribController,
+                      decoration: InputDecoration(
+                        labelText: "RIB/iBAN",
                       ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        controller: ribController,
-                        decoration: InputDecoration(
-                          labelText: "RIB/iBAN",
-                          filled: true,
-                          fillColor: Colors.blue[50], // Couleur de fond du champ de texte
-                        ),
+                    ),
+                    SizedBox(height: 20.0),
+                    TextFormField(
+                      controller: tarifController,
+                      decoration: InputDecoration(
+                        labelText: "Tarif horaire",
                       ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        controller: tarifController,
-                        decoration: InputDecoration(
-                          labelText: "Tarif horaire",
-                          filled: true,
-                          fillColor: Colors.blue[50], // Couleur de fond du champ de texte
-                        ),
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: 20.0),
-              Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                  side: BorderSide(color: Colors.yellow, width: 2),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        'Horaires',
-                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
-                        textAlign: TextAlign.center,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      'Horaires',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 20.0),
+                    DropdownButtonFormField<String>(
+                      value: selectedJour,
+                      onChanged: (newValue) {
+                        setState(() {
+                          selectedJour = newValue;
+                        });
+                      },
+                      items: jours.map((jour) {
+                        return DropdownMenuItem(
+                          value: jour,
+                          child: Text(jour),
+                        );
+                      }).toList(),
+                    ),
+                    SizedBox(height: 20.0),
+                    buildHourInput(debutController),
+                    SizedBox(height: 20.0),
+                    buildHourInput(finController),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: _ajouterHoraire,
+                      child: Text('Ajouter horaire'),
+                      style: ElevatedButton.styleFrom(
+                        primary: AppGlobal.primaryColor,
                       ),
-                      SizedBox(height: 20.0),
-                      DropdownButtonFormField<String>(
-                        value: selectedJour,
-                        onChanged: (newValue) {
-                          setState(() {
-                            selectedJour = newValue;
-                          });
-                        },
-                        items: jours.map((jour) {
-                          return DropdownMenuItem(
-                            value: jour,
-                            child: Text(jour),
-                          );
-                        }).toList(),
-                      ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        controller: debutController,
-                        decoration: InputDecoration(
-                          labelText: "Horaire de début",
-                          filled: true,
-                          fillColor: Colors.blue[50], // Couleur de fond du champ de texte
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      TextFormField(
-                        controller: finController,
-                        decoration: InputDecoration(
-                          labelText: "Horaire de fin",
-                          filled: true,
-                          fillColor: Colors.blue[50], // Couleur de fond du champ de texte
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      ElevatedButton(
-                        onPressed: _ajouterHoraire,
-                        child: Text('Ajouter horaire'),
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.yellow, // Couleur de fond du bouton
-                        ),
-                      ),
-                      SizedBox(height: 20.0),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          for (final horaire in horaires)
-                            Card(
-                              child: ListTile(
-                                title: Text(
-                                  '${horaire['jour']} - Début: ${horaire['debut']}, Fin: ${horaire['fin']}',
-                                  textAlign: TextAlign.center,
-                                ),
+                    ),
+                    SizedBox(height: 20.0),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        for (final horaire in horaires)
+                          Card(
+                            child: ListTile(
+                              title: Text(
+                                '${horaire['jour']} - Début: ${horaire['debut']} heure, Fin: ${horaire['fin']} heure',
+                                textAlign: TextAlign.center,
+                              ),
+                              trailing: IconButton(
+                                icon: Icon(Icons.delete),
+                                onPressed: () {
+                                  setState(() {
+                                    horaires.remove(horaire);
+                                  });
+                                },
                               ),
                             ),
-                        ],
-                      ),
-                    ],
-                  ),
+                          ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: 20.0),
@@ -182,29 +179,21 @@ class _ChangementStatutPageState extends State<ChangementStatutPage> {
                 onPressed: _confirmer,
                 child: Text('Confirmer'),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow, // Couleur de fond du bouton
+                  primary: AppGlobal.primaryColor,
                 ),
               ),
               SizedBox(height: 20.0),
               LinearProgressIndicator(
                 value: _progressValue,
-                backgroundColor: Colors.blue[50], // Couleur de fond de la barre de progression
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.yellow), // Couleur de remplissage de la barre de progression
+                backgroundColor: AppGlobal.backgroundColor,
+                valueColor: AlwaysStoppedAnimation<Color>(AppGlobal.secondaryColor),
               ),
             ],
           ),
         ),
       ),
+      widget,
+      context,
     );
-  }
-
-  @override
-  void dispose() {
-    debutController.dispose();
-    finController.dispose();
-    telephoneController.dispose();
-    ribController.dispose();
-    tarifController.dispose();
-    super.dispose();
   }
 }
