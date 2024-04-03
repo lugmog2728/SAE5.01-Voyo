8475e47d221d99654710e850fd97b5f7c51085de
bensalay <yanis.ben-salah@etu.univ-grenoble-alpes.fr>
2024-04-03 13:38:49 +0200

    Modification de connexion, maintenant in faut un compte
    ex:
    admin.smith@example.com
    motdepasse2

--- voyo/lib/connexion.dart
+++ voyo/lib/connexion.dart
@@ -2,110 +2,158 @@ import 'package:flutter/material.dart';
 import 'globals.dart' as AppGlobal;
 import 'home.dart' as homePage;
 import 'newaccount.dart' as newAccountPage;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class ConnexionPage extends StatefulWidget {
-  const ConnexionPage({super.key, required this.title});
+  const ConnexionPage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
   State<ConnexionPage> createState() => _ConnexionPageState();
 }
-//Obscuretext
+
 class _ConnexionPageState extends State<ConnexionPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _loginError = false;
+
+  Future<void> attemptLogin() async {
+    final email = _emailController.text;
+    final password = _passwordController.text;
+
+    final response = await http.post(
+      Uri.parse('http://172.26.240.10:1080/voyo/User/Connexion'),
+      body: {
+        'email': email,
+        'password': password,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final userData = jsonDecode(response.body);
+      if (userData['Id'] != null) {
+        // Connexion réussie
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => homePage.HomePage(title: 'Accueil'),
+          ),
+        );
+      } else {
+        // Connexion échouée
+        setState(() {
+          _loginError = true;
+        });
+      }
+    } else {
+      // Erreur lors de la requête
+      setState(() {
+        _loginError = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppGlobal.Menu(
-        Wrap(children: [
+      Wrap(
+        children: [
           Form(
             autovalidateMode: AutovalidateMode.always,
             onChanged: () {
               Form.of(primaryFocus!.context!).save();
             },
-            child: Wrap(children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Expanded(
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Identifiant",
-                      filled: true,
-                      fillColor: AppGlobal.buttonback,
-                      border: InputBorder.none,
+            child: Wrap(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Expanded(
+                    child: TextFormField(
+                      controller: _emailController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: "E-mail",
+                        filled: true,
+                        fillColor: AppGlobal.buttonback,
+                        border: InputBorder.none,
+                      ),
+                      onSaved: (String? value) {
+                        debugPrint('Value for field saved as $value');
+                      },
                     ),
-                    onSaved: (String? value) {
-                      debugPrint('Value for field saved as ');
-                    },
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Expanded(
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: "Mot de passe",
-
-                      filled: true,
-                      fillColor: AppGlobal.buttonback,
-                      border: InputBorder.none,
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Expanded(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      textAlign: TextAlign.center,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "Mot de passe",
+                        filled: true,
+                        fillColor: AppGlobal.buttonback,
+                        border: InputBorder.none,
+                      ),
+                      onSaved: (String? value) {
+                        debugPrint('Value for field saved as $value');
+                      },
                     ),
-                    onSaved: (String? value) {
-                      debugPrint('Value for field saved as ');
+                  ),
+                ),
+                if (_loginError)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'E-mail ou mot de passe incorrect',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.yellow,
+                      onPrimary: Colors.white,
+                    ),
+                    onPressed: attemptLogin,
+                    child: const Text(
+                      'Connexion',
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.yellow,
+                      onPrimary: Colors.white,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const newAccountPage.NewAccountPage(
+                            title: "Créer un compte",
+                          ),
+                        ),
+                      );
                     },
+                    child: const Text(
+                      'Créer votre compte',
+                      style: TextStyle(color: Colors.black),
+                    ),
                   ),
                 ),
-              ),
-              Center(
-                  child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow,
-                  onPrimary: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const homePage.HomePage(title: "Acceuil")),
-                  );
-                },
-                child: const Text(
-                  'Connexion',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              )),
-              Center(
-                  child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow,
-                  onPrimary: Colors.white,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            const newAccountPage.NewAccountPage(
-                                title: "Créer un compte")),
-                  );
-                },
-                child: const Text(
-                  'Créer votre compte',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-              ))
-            ]),
+              ],
+            ),
           ),
-        ]),
-        widget,
-        context);
+        ],
+      ),
+      widget,
+      context,
+    );
   }
 }
