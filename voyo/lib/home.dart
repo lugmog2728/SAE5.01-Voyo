f3031c0e0a3301cdf2cbded0f013a7d61674fe20
clementb3 <boulayclement2003@gmail.com>
2024-04-05 09:17:07 +0200

    h

--- voyo/lib/home.dart
+++ voyo/lib/home.dart
@@ -126,6 +126,8 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return AppGlobal.Menu(
       Expanded(
+        Container(
+          child : 
         child:Stack(
         children: [
         Expanded(
